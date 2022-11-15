8ac56fe5c08860cdf2e4cea026b6bb1ddf1e5386
nathanvass <nathanvass@ibm.com>
2022-11-15 15:59:59 -0500

    fail the pipeline

--- src/template.js
+++ src/template.js
@@ -1,1 +1,41 @@
 module.exports = "placeholder";
+const method = () => {
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  2 + 1;
+
+  return "place";
+};
+
+const methodTwo = () => {
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  return "holde";
+};
+
+const methodThree = () => {
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  1 + 1;
+  return "r";
+};
+module.exports = method() + methodTwo() + methodThree();
